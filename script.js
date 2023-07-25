4856a9afa2ccfbcac8dc36f2cbfa92ecf2823e3c
Dhanush Kumar S S <111297810+Dhanush012003@users.noreply.github.com>
2023-07-25 14:18:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function wfun(){
+    document.getElementById("img").setAttribute("src","https://imgd.aeplcdn.com/370x208/n/e4m0qsa_1476916.jpg?q=75")
+}
+function rfun(){
+    document.getElementById("img").setAttribute("src","https://imgd.aeplcdn.com/370x208/n/3fl0qsa_1476914.jpg?q=75")
+}
+function yfun(){
+    document.getElementById("img").setAttribute("src","https://imgd.aeplcdn.com/370x208/n/wzj0qsa_1476912.jpg?q=75")
+}
+data=document.getElementById("buy");
+function bfun(){
+    data.textContent="Ordered Successully";
+}
